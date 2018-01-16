abd54b31cf3806db1c0f7aeef675fad091bdb84c
raulanatol <raul@natol.es>
2018-01-16 20:06:27 +0000

    Cambios color

--- js/index.js
+++ js/index.js
@@ -2,58 +2,59 @@ var login;
 
 
 function validacion(nombre, contrasena) {
-     var contrasenanumero = Number(contrasena);
-     if (nombre === 'valle' && contrasenanumero === 1234) {
-          estoylogueado = true;
-  } else {
-     estoylogueado = false;
-  }
+    var contrasenanumero = Number(contrasena);
+    if (nombre === 'valle' && contrasenanumero === 1234) {
+        estoylogueado = true;
+    } else {
+        estoylogueado = false;
+    }
 }
 
 // carrusel//
 
-function cambiarcolor(color){
-  alert('cambio de color ' + color );
+function cambiarcolor(elementoSelecionado) {
+    var idColor = '#' + $(elementoSelecionado).attr('data-image');
+    $('.fotos > div > img').hide();
+    $(idColor).show();
+    /*
+    if (color === 'dorado') {
+        $('#dorado-img').show();
+        $('#negro-img').hide();
+        $('#rojo-img').hide();
 
-  if (color==='dorado') {
-    $('#dorado-img').show();
+
+    }
+    if (color === 'negro') {
+        $('#negro-img').show();
+        $('#dorado-img').hide();
+        $('#rojo-img').hide();
+    }
+    if (color === 'rojo') {
+        $('#rojo-img').show();
+        $('#negro-img').hide();
+        $('#dorado-img').hide();
+    }
+    */
+}
+
+
+$(document).ready(function () {
+    $('.slider').slick();
+    // $('#dorado-img').hide();
     $('#negro-img').hide();
     $('#rojo-img').hide();
 
+    $('.selector-color i').click(function () {
+        cambiarcolor(this);
+    });
 
-  }
-  if (color==='negro') {
-      $('#negro-img').show();
-      $('#dorado-img').hide();
-      $('#rojo-img').hide();
-    }
-  if (color==='rojo') {
-      $('#rojo-img').show();
-      $('#negro-img').hide();
-      $('#dorado-img').hide();
-        }
-      }
-
-
-
-
-
-
-
-
-$(document). ready(function(){
-  $('.slider').slick();
-  $('#dorado-img').hide();
-  $('#negro-img').hide();
-  $('#rojo-img').hide();
-
-  $("#dorado").click(function() {
-    cambiarcolor('dorado');
-  });
-  $("#negro").click(function() {
-    cambiarcolor('negro');
-  });
-  $("#rojo").click(function() {
-    cambiarcolor('rojo');
-  });
+    // $("#dorado").click(function () {
+    //     cambiarcolor('dorado');
+    // });
+    // $("#negro").click(function () {
+    //     cambiarcolor('negro');
+    // });
+    // $("#rojo").click(function () {
+    //     cambiarcolor('rojo');
+    // });
 });
